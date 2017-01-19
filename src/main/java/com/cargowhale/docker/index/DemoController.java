24353feb4376e3f451c281320f0f21f068cb202a
tclchiam <tchupp@pillartechnology.com>
2017-01-18 23:55:20 -0500

    Added random delay to endpoints to provide more interesting graphs

--- src/main/java/com/cargowhale/docker/index/DemoController.java
+++ src/main/java/com/cargowhale/docker/index/DemoController.java
@@ -8,9 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/api")
 public class DemoController {
+
+    private static final Random RANDOM = new Random();
 
     public class DemoResource {
 
@@ -26,6 +30,7 @@ public class DemoController {
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed(name = "endpoint.happy", absolute = true)
     public ResponseEntity<DemoResource> happy() {
+        randomWait();
         return ResponseEntity.status(HttpStatus.OK).body(new DemoResource("We happy! :)"));
     }
 
@@ -34,6 +39,7 @@ public class DemoController {
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed(name = "endpoint.sad", absolute = true)
     public ResponseEntity<DemoResource> sad() {
+        randomWait();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DemoResource("We sad... :("));
     }
 
@@ -42,6 +48,16 @@ public class DemoController {
         produces = MediaType.APPLICATION_JSON_VALUE)
     @Timed(name = "endpoint.mad", absolute = true)
     public ResponseEntity<DemoResource> mad() {
+        randomWait();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new DemoResource("We mad >:("));
+    }
+
+    private static void randomWait() {
+        double waitTime = 500 * RANDOM.nextGaussian();
+        long startTime = System.currentTimeMillis();
+
+        while (waitTime + startTime > System.currentTimeMillis()) {
+            Thread.yield();
+        }
     }
 }
